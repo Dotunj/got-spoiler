ded4da4aed715f9608f1339ba595d6aebc55aee7
Dotunj <dotunjolaosho@gmail.com>
2017-07-24 09:14:36 +0100

    stuff

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['https://gotspoileralert.com/#/'],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
     'exposedHeaders' => [],
