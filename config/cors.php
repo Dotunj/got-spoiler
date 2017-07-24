14fd23332552377b10714182982a76d4847ae8b1
Dotunj <dotunjolaosho@gmail.com>
2017-07-24 14:36:05 +0100

    thekey

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['https://gotspoileralert.com/'],
     'allowedHeaders' => ['X-Authorization'],
     'allowedMethods' => ['*'],
     'exposedHeaders' => [],
